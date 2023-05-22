eb8719b2945b9770c675c58d2674009578b36c8a
V0pe <dampblew01@gmail.com>
2023-05-22 15:49:00 -0600

    add the application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  resources :inventories
-  resources :inventory_foods
-  resources :foods
-  resources :recipe_foods
-  resources :recipes
-  resources :users
+  resources :inventories, only: [:index, :show, :new, :create, :destroy]
+  resources :foods, only: [:new, :create, :destroy]
+  resources :recipes, only: [:new, :create, :destroy]
+  resources :users, only: [:index, :show]
+
+  # resources :recipe_foods, only: [:new, :create, :destroy]
+  # resources :users, only: [:index, :show] do
+  #   resources :inventories, controller: 'users/inventories' do
+  #     resources :foods, only: [:new, :create, :destroy], controller: 'users/inventories/foods'
+  #     # resources :recipe_foods, only: [:create], controller: 'users/posts/likes'
+  #   end
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
