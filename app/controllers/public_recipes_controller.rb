bf435f3474d68b6f47deb499edaf3d7c357814bc
V0pe <dampblew01@gmail.com>
2023-05-25 21:14:55 -0600

    add the public recipe details

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.where(public: true)
+    @recipe_foods = RecipeFood.where(recipe_id: params[:id])
   end
 end
