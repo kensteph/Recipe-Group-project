39b457fa42e22332020750c5346138ec63a5efcf
V0pe <dampblew01@gmail.com>
2023-05-26 12:40:52 -0600

    add test for recipe index and public recipes

--- spec/system/public_recipes_spec.rb
+++ spec/system/public_recipes_spec.rb
@@ -1,39 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe 'Inventories', type: :request do
+RSpec.describe 'Public Recipes', type: :request do
   describe 'GET /index' do
+    let(:user) do
+      User.create!(
+        name: 'Tom',
+        email: 'test@mail.com',
+        password: '123456'
+      )
+    end
+    let(:recipe) do
+      Recipe.create!(
+        name: 'icecream',
+        preparation_time: 3,
+        cooking_time: 4,
+        description: 'ice cream made easy',
+        public: true,
+        user: user
+      )
+    end
+    before(:each) do
+      get public_recipes_path(user_id: user.id)
+      recipe.save
+    end
+
     it 'renders index template' do
-      get '/inventories'
+      get '/public_recipes'
       expect(response).to render_template(:index)
     end
     it 'renders a successful response' do
-      get '/inventories'
+      get '/public_recipes'
       expect(response).to be_successful
     end
 
-    it 'Test if /users is loading correctly the body' do
-      get '/inventories'
-      expect(response.body).to include('Inventories')
-    end
-  end
-
-  describe 'GET /show' do
-    it 'renders show template' do
-      inventory = Inventory.create(name: 'My Inventory', user_id: 1)
-      get "/inventories/#{inventory.id}"
-      expect(response).to render_template(:show)
+    it 'Test if /public_recipe is loading correctly the body' do
+      get '/public_recipes'
+      expect(response.body).to include('Public Recipes')
     end
 
-    it 'renders a successful response' do
-      inventory = Inventory.create(name: 'My Inventory', user_id: 1)
-      get "/inventories/#{inventory.id}"
-      expect(response).to be_successful
-    end
-
-    it 'Test if /inventories/:id is loading correctly the body' do
-      inventory = Inventory.create(name: 'My Inventory', user_id: 1)
-      get "/inventories/#{inventory.id}"
-      expect(response.body).to include('My Inventory')
+    it 'Test if /recipes has the right recipe created' do
+      get '/public_recipes'
+      expect(response.body).to include('Tom')
+      expect(response.body).to include('icecream')
     end
   end
 end
+
