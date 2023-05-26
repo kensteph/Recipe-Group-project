36f539e046a4b233f492cbed98d23e30a6806f1f
V0pe <dampblew01@gmail.com>
2023-05-26 08:32:00 -0600

    add the recipe test

--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -1,51 +1,57 @@
-require 'application_system_test_case'
+require 'rails_helper'
 
-class RecipesTest < ApplicationSystemTestCase
-  setup do
-    @recipe = recipes(:one)
-  end
+RSpec.describe 'Recipe index/show', type: :system do
+  describe 'Recipe index page' do
+    let(:user) do
+      User.create!(
+        name: 'Tom',
+        email: 'test@mail.com',
+        password: '123456'
+      )
+    end
+    let(:recipe) do
+      Recipe.create!(
+        name: 'icecream',
+        preparation_time: 3,
+        cooking_time: 4,
+        description: 'This is capybara2',
+        public: true,
+        user: user
+      )
+    end
+    before(:each) do
+      visit recipes_path(user_id: user.id)
+      recipe.save
+    end
 
-  test 'visiting the index' do
-    visit recipes_url
-    assert_selector 'h1', text: 'Recipes'
-  end
-
-  test 'should create recipe' do
-    visit recipes_url
-    click_on 'New recipe'
-
-    fill_in 'Cooking time', with: @recipe.cooking_time
-    fill_in 'Description', with: @recipe.description
-    fill_in 'Name', with: @recipe.name
-    fill_in 'Preparation time', with: @recipe.preparation_time
-    check 'Public' if @recipe.public
-    fill_in 'User', with: @recipe.user_id
-    click_on 'Create Recipe'
-
-    assert_text 'Recipe was successfully created'
-    click_on 'Back'
-  end
-
-  test 'should update Recipe' do
-    visit recipe_url(@recipe)
-    click_on 'Edit this recipe', match: :first
-
-    fill_in 'Cooking time', with: @recipe.cooking_time
-    fill_in 'Description', with: @recipe.description
-    fill_in 'Name', with: @recipe.name
-    fill_in 'Preparation time', with: @recipe.preparation_time
-    check 'Public' if @recipe.public
-    fill_in 'User', with: @recipe.user_id
-    click_on 'Update Recipe'
-
-    assert_text 'Recipe was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'should destroy Recipe' do
-    visit recipe_url(@recipe)
-    click_on 'Destroy this recipe', match: :first
-
-    assert_text 'Recipe was successfully destroyed'
+    it 'visiting the index' do
+      visit recipes_url
+      assert_selector 'h1', text: 'Recipes'
+    end
+  
+    it 'should create recipe' do
+      visit recipes_url
+      assign(:current_user, user)
+      click_on 'New recipe'
+  
+      fill_in 'Cooking time', with: recipe.cooking_time
+      fill_in 'Description', with: recipe.description
+      fill_in 'Name', with: recipe.name
+      fill_in 'Preparation time', with: recipe.preparation_time
+      check 'Public' if recipe.public
+      page.find('#recipe_user_id', visible: false).set(user.id)
+      click_on 'Create Recipe'
+  
+      assert_text 'Recipe was successfully created'
+      click_on 'Back to recipes'
+    end
+  
+    it 'should destroy Recipe' do
+      visit recipes_url
+      click_on 'Remove', match: :first
+  
+      # assert_text 'Recipe was successfully destroyed'
+    end
   end
 end
+
